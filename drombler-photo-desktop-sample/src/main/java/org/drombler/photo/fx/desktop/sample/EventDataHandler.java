0ed00f0334281397d2f66f9cbe804bb1caf85a4f
Florian Brunner <fbrunnerreg@gmx.ch>
2018-08-11 00:34:40 +0200

    - clean-up - fixes - EventsViewPane added

---
+++ drombler-photo-desktop-sample/src/main/java/org/drombler/photo/fx/desktop/sample/EventDataHandler.java
@@ -0,0 +1,36 @@
+package org.drombler.photo.fx.desktop.sample;
+
+import java.util.UUID;
+import org.drombler.acp.core.data.AbstractDataHandler;
+import org.drombler.acp.core.data.BusinessObjectHandler;
+import org.drombler.event.core.Event;
+
+/**
+ *
+ * @author Florian
+ */
+@BusinessObjectHandler
+public class EventDataHandler extends AbstractDataHandler<UUID> {
+
+    private final Event event;
+
+    public EventDataHandler(Event event) {
+        this.event = event;
+    }
+
+    @Override
+    public String getTitle() {
+        return event.getName();
+    }
+
+    @Override
+    public String getTooltipText() {
+        return event.getName() + "(" + event.getDuration() + ")";
+    }
+
+    @Override
+    public UUID getUniqueKey() {
+        return event.getId();
+    }
+
+}
